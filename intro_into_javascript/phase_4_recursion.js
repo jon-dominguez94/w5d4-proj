5e4905b22b8483deb5f3e709eb06142741cc1c27
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-11-22 15:39:10 -0800

    Implement #sumRec

--- intro_into_javascript/phase_4_recursion.js
+++ intro_into_javascript/phase_4_recursion.js
@@ -6,4 +6,16 @@ function range (start, end) {
   return [start].concat(range(start + 1, end));
 }
 
-console.log(range(1,5));
+// console.log(range(1,5));
+
+function sumRec(arr) {
+  if (arr.length === 1) {
+    return arr[0];
+  }
+  
+  return arr[0] + sumRec(arr.slice(1,arr.length+3));
+}
+
+let arr4 = [1,2,3];
+
+console.log(sumRec(arr4));
