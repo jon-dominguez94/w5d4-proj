68c2904e4bc3b77e24b08315e2ae46cdbf14f21d
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-11-22 15:47:06 -0800

    Implement exponents in recursion

--- intro_into_javascript/phase_4_recursion.js
+++ intro_into_javascript/phase_4_recursion.js
@@ -16,6 +16,33 @@ function sumRec(arr) {
   return arr[0] + sumRec(arr.slice(1,arr.length+3));
 }
 
-let arr4 = [1,2,3];
+// let arr4 = [1,2,3];
 
-console.log(sumRec(arr4));
+// console.log(sumRec(arr4));
+
+function exp1(base, exp) {
+  if (exp === 0) {
+    return 1;
+  }
+  return base * exp1(base, exp - 1);
+}
+
+// console.log(exp1(4,2));
+
+function exp2(base, exp) {
+  if (exp === 0) {
+    return 1;
+  }
+  if (exp === 1) {
+    return base;
+  }
+  
+  if (exp % 2 === 0) {
+    return exp2(base, exp/2) * exp2(base, exp/2);
+  } else {
+    return base * (exp2(base, (exp - 1)/2));
+  }
+}
+
+console.log(exp2(4,2));
+console.log(exp2(5,2));
